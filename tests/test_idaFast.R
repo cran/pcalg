032fd6d1c51579a784f893a4c4838b0381dc9830
Markus Kalisch <kalisch@stat.math.ethz.ch>
2022-12-20 23:20:05 +0000

    version 2.7-8

--- tests/test_idaFast.R
+++ tests/test_idaFast.R
@@ -1,41 +1,49 @@
 library(pcalg)
 suppressWarnings(RNGversion("3.5.0"))
-set.seed(43)
-nreps <- 50
-res <- matrix(FALSE, 3, nreps)
-resF <- rep(FALSE, nreps)
 
-for (i in 1:nreps) {
-  if (i%%100 == 0) cat("i=",i,"\n")
+p <- 10
+ip <- seq_len(p) # 1:p = 1 2 .. p
+
+one.equal <- function(x1, xx, tol = 1e-14) {
+    stopifnot(length(x1) == 1L, length(xx) >= 1L)
+    ## the one xx[] that is close to x1 :
+    x. <- xx[which.min(abs(xx - x1))]
+    (if(abs(x1) < 1e-100) abs(x.- x1) else abs(x./x1 - 1)) <= tol
+}
+
+for (i in 1:50) {
+  cat("i=",i,"\n--==\n",sep="") # need to see where it fails (if ..)
   set.seed(i)
-  p <- 10
   ## generate and draw random DAG :
   myDAG <- randomDAG(p, prob = 0.4)
   myCPDAG <- dag2cpdag(myDAG)
   mcov <- trueCov(myDAG)
 
-  x <- sample(1:10,1)
-  y1 <- sample(setdiff(1:10,x),1)
-  y2 <- sample(setdiff(1:10,c(x,y1)),1)
-  y3 <- sample(setdiff(1:10,c(x,y1,y2)),1)
+  x  <- sample(ip, 1)
+  y1 <- sample(setdiff(ip,  x),       1)
+  y2 <- sample(setdiff(ip,c(x,y1)),   1)
+  y3 <- sample(setdiff(ip,c(x,y1,y2)),1)
   ## plot(myCPDAG)
-  eff.true1 <- round(causalEffect(myDAG, y1, x),14)
-  eff.true2 <- round(causalEffect(myDAG, y2, x),14)
-  eff.true3 <- round(causalEffect(myDAG, y3, x),14)
+  eff.true1 <- causalEffect(myDAG, y1, x)
+  eff.true2 <- causalEffect(myDAG, y2, x)
+  eff.true3 <- causalEffect(myDAG, y3, x)
   ## cat("x=",x," y1=",y1," eff=",eff.true1,"\n")
   ## cat("x=",x," y1=",y2," eff=",eff.true2,"\n")
 
-  (eff.est1 <- round(ida(x,y1,mcov,myCPDAG,method="local",verbose=FALSE),14))
-  (eff.est2 <- round(ida(x,y2,mcov,myCPDAG,method="local",verbose=FALSE),14))
-  (eff.est3 <- round(ida(x,y3,mcov,myCPDAG,method="local",verbose=FALSE),14))
-  (eff.estF <- round(idaFast(x,c(y1,y2,y3),mcov,myCPDAG),14))
+  ## cat("est1: "); print(eff.est1 <- ida (x, y1, mcov,myCPDAG, method="local"))
+  ## cat("est2: "); print(eff.est2 <- ida (x, y2, mcov,myCPDAG, method="local"))
+  ## cat("est3: "); print(eff.est3 <- ida (x, y3, mcov,myCPDAG, method="local"))
+  eff.est1 <- ida (x, y1, mcov,myCPDAG, method="local")
+  eff.est2 <- ida (x, y2, mcov,myCPDAG, method="local")
+  eff.est3 <- ida (x, y3, mcov,myCPDAG, method="local")
+  eff.estF <- idaFast(x, c(y1,y2,y3), mcov,myCPDAG)
+  cat("estF:\n"); print(eff.estF)
 
-  res[1,i] <- (eff.true1 %in% eff.est1)
-  res[2,i] <- (eff.true2 %in% eff.est2)
-  res[3,i] <- (eff.true3 %in% eff.est3)
-
-  resF[i] <- all(eff.estF == rbind(eff.est1,eff.est2,eff.est3))
+  stopifnot(exprs = {
+      one.equal(eff.true1, eff.est1)
+      one.equal(eff.true2, eff.est2)
+      one.equal(eff.true3, eff.est3)
+      all.equal(unname(eff.estF), tol = 3e-15, # even tol=0  works on some
+                rbind(eff.est1, eff.est2, eff.est3, deparse.level=0L))
+  })
 }
-
-if (!all(res)) stop("Test idaFast: True effects were not recovered!")
-if (!all(resF)) stop("Test idaFast: ida and idaFast didn't give same results!")
